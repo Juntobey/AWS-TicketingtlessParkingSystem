4938651028a22f2cb4aff6e26b44f7096ab5721f
Juntobey <teelee0811@gmail>
2026-07-23 22:55:28 +0200

    Creating the home page and the navbar component for the ticketless parking system application. The App.jsx file has been modified to import and render the Home component, while the Navbar.jsx file has been created to define the navigation bar structure.

---
+++ ticketingless-parking-system/src/Home.jsx
@@ -0,0 +1,15 @@
+import Navbar from './components/Navbar';
+function Home() {
+    return(
+        <div>
+            <Navbar />
+            <main>
+                <h1>Welcome to the Home Page</h1>
+                <p>This is the home page content.</p>
+            </main>
+        </div>
+    );
+
+}
+
+export default Home;
---
+++ ticketingless-parking-system/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+// If the filename contains spaces or dashes, ensure the actual file path matches this string.
+function Navbar(){
+    return(
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    )
+
+}
+
+export default Navbar;
